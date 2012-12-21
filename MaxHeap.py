14453c0b3951dfab1addcdfa2aa59061423a9d07
bolshchikov <sergey@bolshchikov.net>
2012-12-21 18:17:23 +0200

    all methods are implemented

--- MaxHeap.py
+++ MaxHeap.py
@@ -57,22 +57,41 @@ class MaxHeap:
 
         while new_pos != 1 and self.compareProcesses(self._array[new_pos], self._array[self.parent(new_pos)]) > 0:
             self.swap_(new_pos, self.parent(new_pos))
-            new_pos = parent(new_pos)
+            new_pos = self.parent(new_pos)
         return new_pos
 
 
-    def increaseKey(self, index):
+    def increaseKey(self, index, key):
         '''
         The increaseKey function updates the heap with a process with 
         a larger priority in index.
         The method assumes that the new process's key is larger than 
         the key of the current process at array[index].
         '''
-        pass
+        if self._array[index] == None:
+            print 'increaseKey: process with index %d does not exist in the heap \n' % index
+        if key < self._array[index]:
+            print 'increaseKey: new key is smaller than current key \n'
+            return
+        self._array[index] = key
+        while index > 1 and self.compareProcesses(self._array[self.parent(index)], self._array[index]) < 0:
+            self.swap_(index, self.parent(index))
+            index = self.parent(index)   
+
     def remove_max(self):
-        pass
-    def remove(self, member):
-        pass
+        if self.IsEmpty():
+            print 'remove_max: heap is empty \n'
+            return -1
+        maximum = self._array[1]
+        self._array[1] = self._array[self._elements]
+        self._elements -= 1
+        self.heapify(1)
+        return maximum
+
+    def remove(self, index):
+        self.swap_(index, 1);
+        self.remove_max();
+
     def IsEmpty(self):
         return self._elements == 0
 
@@ -95,7 +114,7 @@ class MaxHeap:
         You must decide on the criteria for comparing the processes.
         '''
         if process1 == None or process2 == None:
-            print 'compareProcesses: processes must have priorities'
+            print 'compareProcesses: processes must have priorities \n'
             return
         if process1 == process2:
             return 0
@@ -130,7 +149,7 @@ class MaxHeap:
 
     def swap_(self, index1, index2):
         if index1 < 1 or index1 > self._elements or index2 < 1 or index2 > self._elements:
-            print 'swap: invalid indexes %d %d \n' % index1 % index2
+            print 'swap: invalid indexes %d and %d \n' % (index1, index2)
             return
         temp = self._array[index1]
         self._array[index1] = self._array[index2]
@@ -148,7 +167,13 @@ def main():
     heap.insert(2)
     heap.insert(4)
     heap.insert(1)
-    print heap._array[heap.parent(2)]
+    
+    heap.printHeap()
+    heap.increaseKey(4, 15)
+    heap.printHeap()
+    heap.remove(3)
+    heap.printHeap()
+    print heap._elements
 
 if __name__ == '__main__':
     main()
